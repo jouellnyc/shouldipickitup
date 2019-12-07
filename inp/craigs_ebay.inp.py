1701234cacf2ad411a9e21c19c31b91c5c199e16
jouellnyc <jouellnyc@gmail.com>
2019-12-06 21:20:28 -0500

    update

--- inp/craigs_ebay.inp.py
+++ inp/craigs_ebay.inp.py
@@ -27,15 +27,11 @@ citytext = f"{city},{state}"
 craigs_list_url = craigzipsandurls.lookup_craigs_url(citytext).decode('UTF-8')
 print(f"{citytext} is available at {craigs_list_url}")
 
-sys.exit()
-
 
 start  = '40.6490763'
 end    = '-73.9762069'
 
-myzip = '32613'
-
-craigs_main_url   = f'https://newyork.craigslist.org/search/brk/zip?postal={myzip}'
+craigs_main_url   = craigs_list_url + "/d/free-stuff/search/zip"
 craigs_main_resp  = requestwrap.err_web(craigs_main_url)
 craigs_main_soup  = BeautifulSoup(craigs_main_resp.text, 'html.parser')
 craigs_main_posts = craigs_main_soup.find_all('a', class_= 'result-title hdrlnk')
@@ -45,7 +41,7 @@ lyft_url     = "http://www.lyft.com"
 ebay_url     = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw="
 
 
-for each_item in craigs_main_posts:
+for each_item in craigs_main_posts[0:5]:
 
     item_url          = each_item.attrs['href']
     craigs_resp       = requestwrap.err_web(item_url)
@@ -62,10 +58,16 @@ for each_item in craigs_main_posts:
     ebay_query_url = ebay_url + ebay_path
     ebay_resp      = requestwrap.err_web(ebay_query_url)
     ebay_soup      = BeautifulSoup(ebay_resp.text, 'html.parser')
-    item           = ebay_soup.find("h3", {"class": "s-item__title"}).get_text(separator=u" ")
-    price          = ebay_soup.find("span", {"class": "s-item__price"}).get_text()
-
-
-
-    print (f"\"{each_item.text}\" is Free on Craigslist, is selling for {price}"
+    #print("soup eb: " + str( len(ebay_soup) ) )
+    try:
+        item           = ebay_soup.find("h3", {"class": "s-item__title"}).get_text(separator=u" ")
+    except AttributeError:
+        item = "no match"
+    #print("soup i: " + str( len(item)      ) )
+    #print("eb: " + item)
+    try:
+        price          = ebay_soup.find("span", {"class": "s-item__price"}).get_text()
+    except AttributeError:
+        price = "no price"
+    print (f"\"{each_item.text}\" is Free on Craigslist in {city}, and is selling for {price}"
            f" on Ebay and is {miles:.2f} miles away from you.")
