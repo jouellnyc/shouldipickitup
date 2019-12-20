16b683b1c714022022fb815de134a27c5ebb6fe9
jouellnyc <jouellnyc@gmail.com>
2019-12-20 15:22:36 -0500

    updates

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 #!/home/john/anaconda3/bin/python3
 
-'''
 
+'''
 https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
-
 '''
-
-import os
-import sys
-import time
-import pickle
 
 from flask import Flask
 from flask import request
+from flask import render_template
+
+import main
 
 app = Flask(__name__)
 app.debug = True
@@ -24,10 +21,15 @@ def hi():
 
 @app.route('/forms/', methods=['POST', 'GET'])
 def get_data():
-        try:
-            return str(request.form.get('zip'))
-        except Exception as e:
-            return str(e)
+    zip       = str(request.form.get('zip'))
+    items     = main.main(zip)
+    len_items = list(range(0,len(items) ))
+    #return items
+    return render_template('craig_list_local_items.html', len_items=len_items,items=items, zip=zip)
+    #try:
+    #    return str(request.form.get('zip'))
+    #except Exception as e:
+    #    return str(e)
 
 if __name__ == "__main__":
     dir(app.run)
