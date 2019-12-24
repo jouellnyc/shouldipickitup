b3fdd944f5cbb2b3c019d7dce787750ee4e81fcf
jouellnyc <jouellnyc@gmail.com>
2019-12-24 00:49:05 -0500

    updates

--- lib/govzipsandcities.py
+++ lib/govzipsandcities.py
@@ -4,8 +4,8 @@
 ''' make available functions to return that data                           '''
 
 ''' Specifically this module only cares about zipcode to citytext:         '''
-'''        '32546' => 'gainesville,FL'
-'''        At the moment about 40k ; i.e 86305, but not 86306               '''
+'''        '32546' => 'gainesville,FL'                                     '''
+'''        At the moment about 40k ; i.e 86305, but not 86306              '''
 ''' cities will be loaded lowercase into the local file and Memcached      '''
 ''' STATES will be UPPER in both                                           '''
 
@@ -23,7 +23,7 @@ zip_code_file = '/home/john/gitrepos/shouldipickitup/data/free-zipcode-database-
 def create_zips_city_state_dict_from_file(zip_code_file):
     ''' Return a dictionary with zip : (city,state) tuples  '''
     ''' give the file at URL                                '''
-
+    print('create1')
     with open(zip_code_file) as csv_fh:
             myzips = {}
             csv_reader = csv.reader(csv_fh, delimiter=',')
@@ -39,16 +39,19 @@ def create_zips_city_state_dict_from_file(zip_code_file):
 
     return myzips
 
-def load_zips_to_memcached(zipcode_dict):
-    ''' write all the key/values to memcached '''
-    client = base.Client(('localhost', 11211))
-    for zip,(city,state) in zipcode_dict.items():
-        client.set(zip,(city,state))
+def generate_all_possible_closest_zips():
+    closest_city_state = {}
+    print('genall2')
+    for zip in ("%.5d" % x for x in range(99999)):
+        print(f"{zip}:", end='')
+        city, state       = lookup_city_state_given_zip_file(zip,myzips)
+        print(city, state)
+        closest_city_state[zip] = (city,state)
+    return closest_city_state
 
-def lookup_city_state_given_zip_file(zip,zip_code_file):
+def lookup_city_state_given_zip_file(zip, zip_code_dict):
     ''' Given a zipcode;  return closest city,state zipcode dictionary file '''
-    myzips  = create_zips_city_state_dict_from_file(zip_code_file)
-    closest = myzips[min(myzips.keys(), key=lambda k: abs(int(k)-int(zip)))]
+    closest = zip_code_dict[min(myzips.keys(), key=lambda k: abs(int(k)-int(zip)))]
     city, state = closest
     return (city,state)
 
@@ -68,6 +71,13 @@ def lookup_city_state_given_zip_memcached(zip):
         state = patt.search(state).group()
         return (city,state)
 
+def load_zips_to_memcached(zipcode_dict):
+    ''' write all the key/values to memcached '''
+    print('load3')
+    client = base.Client(('localhost', 11211))
+    for zip,(city,state) in zipcode_dict.items():
+        client.set(zip,(city,state))
+
 if __name__ == "__main__":
 
     import sys
@@ -75,24 +85,28 @@ if __name__ == "__main__":
 
     try:
         zip = sys.argv[1]
-        myzips = create_zips_city_state_dict_from_file(zip_code_file)
-        #print(myzips)
+        if sys.argv[1] == 'load':
+            print('Please wait')
+            #zip = sys.argv[2]
+            myzips              = create_zips_city_state_dict_from_file(zip_code_file)
+            closest_city_states = generate_all_possible_closest_zips()
+            #print(lookup_city_state_given_zip_file(zip, myzips))
+            load_zips_to_memcached(closest_city_states)
+            #print(lookup_city_state_given_zip_memcached(zip))
+        else:
+            try:
+                zip = sys.argv[1]
+                city,state = lookup_city_state_given_zip_memcached(zip)
+                print(city,state)
+            except Exception as e: #Catches ConnectionRefusedError
+                print("Memcached seems down; going to file:",e)
+                try:
+                    city, state = lookup_city_state_given_zip_file(zip, zip_code_file)
+                    city, state = city.lower(),state.lower()
+                    print(city,state)
+                except OSError as e:
+                    print("OSError",e)
     except OSError as e:
         print("OSError",e)
     except IndexError as e:
         print("Did you specify a zip?")
-    else:
-        try:
-            load_zips_to_memcached(myzips)
-            city,state = lookup_city_state_given_zip_memcached(zip)
-            print(city,state)
-        except Exception as e: #Catches ConnectionRefusedError
-            print("Memcached seems down; going to file:",e)
-            try:
-                city, state = lookup_city_state_given_zip_file(zip, zip_code_file)
-                city, state = city.lower(),state.lower()
-                print(city,state)
-            except OSError as e:
-                print("OSError",e)
-else:
-    from . import app_logger
