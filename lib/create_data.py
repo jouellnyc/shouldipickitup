68f3408a9734125b7f5b14dea481da203130dd8f
jouellnyc <jouellnyc@gmail.com>
2020-01-03 22:06:51 -0500

    push daily

--- lib/create_data.py
+++ lib/create_data.py
@@ -4,8 +4,9 @@ import os
 import re
 import csv
 import sys
-
 import json
+from collections import defaultdict
+
 from pymongo import MongoClient
 
 '''
@@ -18,17 +19,17 @@ Zip code, City, State, Craigslist Url strategy:
 
 Take 2 files:
 
-free-zipcode-database-Primary.no.header.csv - zip codes to City, State generate_documents_to_import_to_mongodb
-craigs_links.txt = City, State names to Craigslist zips2knowncityurls
+free-zipcode-database-Primary.no.header.csv - zip codes to City, State
+craigs_links.txt - City, State names to Craigslist City zips2knowncityurls
 
-Create 2 dictionaries:
+Then create 2 dictionaries:
 
-#Zip to city
+#Zip to city state
 11111-> boston ma
 
 and
 
-#city to craigslist url
+#city, state to craigslist url
 boston,ma -> link.craig.com
 
 #Set up a table from those 2 dictionaries with details:
@@ -45,12 +46,12 @@ boston,ma -> link.craig.com
 #This also means Brooklyn could be considered close to Albany...(well, kind of, for now)
 
 #Round 3
-Find out all the other mappings w/o commas and load those.
-
+Set craigs_url as the key and insert all the associated zip as values in the same
+document. Query for a member of the array to get the items.
 
 #Round 4
+Find out all the other mappings w/o commas and load those.
 Load PR and any other remote sites
-
 '''
 
 URL = 'http://federalgovernmentzipcodes.us/download.html'  # not used
@@ -58,25 +59,22 @@ my_file_name = os.path.basename(__file__)
 zip_code_file = '/home/john/gitrepos/shouldipickitup/data/free-zipcode-database-Primary.no.header.csv'
 craigs_links_file = '/home/john/gitrepos/shouldipickitup/data/craigs_links.txt'
 
-
-def create_zips_city_state_dict_from_local_file(zip_code_file):
-    ''' Return a dictionary with zip : (city,state) tuples  '''
+def create_city_state_zips_dict_from_local_file(zip_code_file):
+    ''' Return a dictionary with (city,state) zip values    '''
     ''' give the file at URL                                '''
+    gov_city_state_zips = defaultdict(list)
     with open(zip_code_file) as csv_fh:
-        city_state_zips = {}
         csv_reader = csv.reader(csv_fh, delimiter=',')
-
         for row in csv_reader:
-            zip = row[0]
+            zipc = row[0]
             city = row[2]
             city = city.lower()
-            city = "".join(city.split())  # no spaces
+            city = " ".join(city.split())  # no spaces
             state = row[3]
+            state = " ".join(state.split())
             state = state.upper()
-            city_state_zips[zip] = (city, state)
-
-    return city_state_zips
-
+            gov_city_state_zips[f"{city},{state}"].append(zipc)
+        return gov_city_state_zips
 
 def create_craigs_url_dict_from_local_file(craigs_links_file):
     ''' Return a dictionary (city,state => url)             '''
@@ -88,60 +86,25 @@ def create_craigs_url_dict_from_local_file(craigs_links_file):
             citytext, craigs_link = line.split("=")
             craigs_link = ''.join(craigs_link.split())
             craigs_city_links[citytext] = craigs_link
+
     return craigs_city_links
 
+def lookup_craigs_url_with_city(citystate, craigs_city_links):
+    return craigs_city_links[citystate]
 
-def lookup_closest_craigs_url_given_zip_with_known_link(zip, city_state_zips):
-    ''' Given a zipcode;  return URL of a zip with a known Craigslist URL '''
-    try:
-        closest = city_state_zips[min(
-            city_state_zips.keys(), key=lambda k: abs(int(k)-int(zip)))]
-        return closest
-    except TypeError as e:
-        raise ValueError(e)
+def generate_master_documents_import_to_mongodb(craigs_city_links, gov_city_state_zips):
 
-def lookup_craigs_url_given_city(citytext, craigs_city_links):
-    return craigs_city_links[citytext]
-
-def lookup_city_state_given_zip(zip, zip_code_dict):
-    return zip_code_dict[zip]
-
-def lookup_craigs_url_given_zip(zip, zip_code_dict):
-    return zip_code_dict[zip]
-
-def create_map_of_zips_to_known_city_urls(craigs_city_links, gov_city_state_zips):
-    ''' Given the 2 mappings, combine where you can pull out a craiglist url '''
-    city_state_zip_map = {}
-    for zip in (gov_city_state_zips.keys()):
-        city, state = lookup_city_state_given_zip(zip, gov_city_state_zips)
-        citytext = city + ',' + state
-        try:
-            city_state_zip_map[zip] = craigs_city_links[citytext]
-        except KeyError:
-            url = None
-    return city_state_zip_map
-
-
-def generate_documents_to_import_to_mongodb(zips2knowncityurls, gov_city_state_zips):
-    ''' Format the data for importing to Mongodb                              '''
-    ''' If a url is not found, find the closest zip and use that url          '''
     mongo_city_state_zip_map = []
 
-    for zip in (gov_city_state_zips.keys()):
-
+    for citystate, ziplist in gov_city_state_zips.items():
         try:
-            url = lookup_craigs_url_given_zip(zip, zips2knowncityurls)
+            url = lookup_craigs_url_with_city(citystate, craigs_city_links)
         except KeyError:
-            url = lookup_closest_craigs_url_given_zip_with_known_link(
-                zip, zips2knowncityurls)
-
-        city, state = lookup_city_state_given_zip(zip, gov_city_state_zips)
-        mongo_doc = {'zip': zip, 'City': city,'State': state, 'craigs_url' : url}
+            url = None
+        mongo_doc = ({'craigs_url': url , "zips" : ziplist, "CityState": citystate })
         print(mongo_doc)
         mongo_city_state_zip_map.append(mongo_doc)
-
     return mongo_city_state_zip_map
-
 
 if __name__ == "__main__":
 
@@ -149,17 +112,12 @@ if __name__ == "__main__":
     import mongodb
 
     try:
-        # Round 1
-        gov_city_state_zips = create_zips_city_state_dict_from_local_file(
-            zip_code_file)
-        craigs_city_links = create_craigs_url_dict_from_local_file(
-            craigs_links_file)
-        zips2knowncityurls = create_map_of_zips_to_known_city_urls(
-            craigs_city_links, gov_city_state_zips)
-        # Round 2
-        mongo_city_state_zip_map = generate_documents_to_import_to_mongodb(
-            zips2knowncityurls, gov_city_state_zips)
+        #Round 3
+        craigs_city_links = create_craigs_url_dict_from_local_file(craigs_links_file)
+        gov_city_state_zips = create_city_state_zips_dict_from_local_file(zip_code_file)
+        mongo_city_state_zip_map = generate_master_documents_import_to_mongodb(
+            craigs_city_links,gov_city_state_zips)
         mongodb.init_load_city_state_zip_map(mongo_city_state_zip_map)
-        # This takes about 2-3 minutes
+        # This takes about 15 seconds now!
     except Exception as e:
         print(e)
--- lib/mongodb.py
+++ lib/mongodb.py
@@ -2,11 +2,11 @@
 
 from pymongo import MongoClient
 
-database_name = "shoulidpickitup"
+database_name = "shouldipickitup"
 collection_name = "data"
 
 #def ConnectToMongo(database_name=database_name, collection_name=collection_name):
-def ConnectToMongo(database_name="shoulidpickitup", collection_name="data"):
+def ConnectToMongo(database_name="shouldipickitup", collection_name="data"):
     client = MongoClient()
     database_handle = client[database_name]
     collection_handle = database_handle[collection_name]
@@ -43,16 +43,20 @@ def lookup_craigs_posts(zip):
         return ([response['Item1'], response['Item2'], response['Item3'] , response['Item4']],
                 [response['Url1'] , response['Url2'] , response['Url3']  ,  response['Url4']])
 
-
 def update_one_document(mongo_filter, mongo_doc):
     """ update one doc to mongodb """
     dbh = ConnectToMongo()
     new_result = dbh.update_one(mongo_filter, mongo_doc)
     print(new_result.raw_result)
 
+def insert_one_document(mongo_filter, mongo_doc):
+    """ update one doc to mongodb """
+    dbh = ConnectToMongo()
+    new_result = dbh.insert_one(mongo_filter, mongo_doc)
+    print(new_result.inserted_id)
 
-def init_load_city_state_zip_map(closest_list):
+def init_load_city_state_zip_map(mongo_city_state_zip_map):
     """ write all the key/values to mongodb """
     dbh = ConnectToMongo()
-    new_result = dbh.insert_many(closest_list)
-    print("Multiple posts: {0}".format(new_result.inserted_ids))
+    new_result = dbh.insert_many(mongo_city_state_zip_map)
+    #print("Multiple posts: {0}".format(new_result.inserted_ids))
