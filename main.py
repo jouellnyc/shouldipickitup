e42d98d5ab7dadc049ff050c46ea3c3ffcc467e8
jouellnyc <jouellnyc@gmail.com>
2020-01-07 17:06:11 -0500

    Docstring update

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+main.py - interface into flask and mongodb
+
+- This script takes in a zip code from Flask/app.py or via cmd line, and then
+determines the right Craiglist URL by qurying 'Zips' or 'AltZips' in MongoDB.
+It then returns the free items (see below) associated with that MongoDB doc.
+
+- If no data matches or if MongoDB errors, S.F data will be returned
+
+-This script requires the mongodb helper module.
+
+-This file can also be imported as a module and contains the following
+functions:
+
+    * main - the main function of the script
+
+TBD: Input validation. Better outcome if MongoDB is down.
+""""
+
 import logging
 
 from lib import mongodb
-from lib import websitepuller
 
 def main(zip):
+    """
+    Parameters
+    ----------
+    zip : str
+        zip code the user entered - inbound from Flask or from cmd line.
+
+    Returns
+    -------
+    all_posts
+        A [list] of all the local posts in the free sections
+    all_links
+        A [list] of all the local posts in the free sections
+    city
+        [str] - the city associated with the zip (for display only)
+    state
+        [str] - the state associated with the zip (for display only)
+
+    start_lat \
+    start_lng /   to be used to calculate distance (disabled for now)
+
+    """
 
     start_lat = "40.6490763"
     start_lng = "-73.9762069"
@@ -14,9 +53,8 @@ def main(zip):
     start_lat = "29.5964"
     start_lng = "-82.2178"
 
-    """ Given a zip, find the Craigslist Url """
     try:
-
+        """ Given a zip, find the Craigslist Url """
         city, state, url, Items, Urls = \
             mongodb.lookup_craigs_url_citystate_and_items_given_zip(zip)
         city, state = city.capitalize(), state.upper()
