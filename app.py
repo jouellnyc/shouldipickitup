2a66107b9569464fb257c90ab76b53577d84dc3f
John Ouellette <jouellnyc@gmail.com>
2020-02-10 20:35:08 +0000

    explicity state -b in  Dockerfile

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6
 
 TBD: - Add js input validation
      - Add cool look ahead form query
+
+REM - https://stackoverflow.com/questions/58464052/request-to-flask-application-does-not-get-through-to-docker-container
 """
 
 
