146161a657626217f8a0ee8cdfb45bcfbd64498a
jouellnyc <jouellnyc@gmail.com>
2020-02-14 18:49:53 -0500

    --# of urls

--- flask-docker-entrypoint.sh
+++ flask-docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /shouldipickitup/
-/usr/local/bin/gunicorn -b 0.0.0.0:8000 app:app -w 4 --access-logfile access.log &
 cd /shouldipickitup/lib
 ./create_data.py
-cd /shouldipickitup/non-app
-./crawl.sh
+#cd /shouldipickitup/non-app
+#./crawl.sh
+cd /shouldipickitup/
+/usr/local/bin/gunicorn -b 0.0.0.0:8000 app:app -w 4 --access-logfile access.log
