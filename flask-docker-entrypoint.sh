35d91ec4ede9426be37a083ef325a043a3518be3
jouellnyc <jouellnyc@gmail.com>
2020-02-16 21:28:58 -0500

    fix entrypoint

--- flask-docker-entrypoint.sh
+++ flask-docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /shouldipickitup/lib
-./create_data.py
-#cd /shouldipickitup/non-app
-#./crawl.sh
+./create_data.py && touch create_data.done
 cd /shouldipickitup/
 /usr/local/bin/gunicorn -b 0.0.0.0:8000 app:app -w 4 --access-logfile access.log
