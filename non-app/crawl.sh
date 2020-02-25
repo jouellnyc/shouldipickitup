66384bc52aafd888829f42eefaee8c23be54f707
jouellnyc <jouellnyc@gmail.com>
2020-02-25 15:04:05 -0500

    ++docs

--- non-app/crawl.sh
+++ non-app/crawl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-while read line; do 
+CITIES=known_cities.txt
+
+while read line; do
 
     echo == $line ==
     date
     SLEEP=$((1 + RANDOM % 30))
-    ../lib/crawler.py $line index; sleep $SLEEP; 
+    ../lib/crawler.py $line index; sleep $SLEEP;
 
-done < <(head -n 5 urls)
-#done < urls 
-
+done < <(head -n 5 $CITIES)
+#done < $CITIES
