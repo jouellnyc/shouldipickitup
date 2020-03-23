e5483d3c84f9299b838983f76316d021ebc35ec5
jouellnyc <jouellnyc@gmail.com>
2020-03-23 19:38:33 -0400

    Update per AWS

--- user_data.http.AWS.sh
+++ user_data.http.AWS.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 yum update -y
+
+yum -y install git
 yum -y install awslogs
+
 amazon-linux-extras install docker
 
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
@@ -13,4 +16,9 @@ chkconfig docker on
 service awslogsd start
 chkconfig awslogsd on
 
+GIT_DIR="/gitrepos/"
+mkdir -p $GIT_DIR
+cd $GIT_DIR/
+git clone https://github.com/jouellnyc/shouldipickitup.git
+cd shouldipickitup
 docker-compose -f docker-compose.AWS.hosted.MongoDb.yaml up -d
