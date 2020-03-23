a4a5dcfe55fe0b8e72ab2e21f2aa40f694cd0bed
jouellnyc <jouellnyc@gmail.com>
2020-03-22 22:28:37 -0400

    Update per AWS

--- lib/mongodb.py
+++ lib/mongodb.py
@@ -79,7 +79,7 @@ class MongoCli:
         """
         try:
             client = MongoClient(
-                    host="mongodb+srv://MONGOUSERNMAE:MONGOPASSWORD@MONGOHOST/test?retryWrites=true&w=majority",
+                    host="mongodb+srv://MONGOUSERNAME:MONGOPASSWORD@MONGOHOST/test?retryWrites=true&w=majority",
                 serverSelectionTimeoutMS=2000,
             )
             client.server_info()
