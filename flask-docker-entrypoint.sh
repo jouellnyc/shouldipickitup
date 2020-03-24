a5bc83272f9f5888084e5c57c030aa84e1ac4675
jouellnyc <jouellnyc@gmail.com>
2020-03-23 23:00:23 -0400

    One Click Deploy!

--- flask-docker-entrypoint.sh
+++ flask-docker-entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-cd /tmp/ && git clone https://github.com/jouellnyc/AWS.git && cd AWS/boto3
-read -r  MONGOUSERNAME MONGOPASSWORD MONGOHOST <  <(/usr/local/bin/python3 ./getSecret.py)
-cd /shouldipickitup/lib/
-
-sed -i s"/MONGOUSERNAME/${MONGOUSERNAME}/" mongodb.py
-sed -i s"/MONGOPASSWORD/${MONGOPASSWORD}/" mongodb.py
-sed -i         s"/MONGOHOST/${MONGOHOST}/" mongodb.py
-
+cd /shouldipickitup/lib
 ./create_data.py && touch create_data.done
-
-cd /shouldipickitup/
-
 /usr/local/bin/gunicorn -b 0.0.0.0:8000 app:app -w 4 --access-logfile access.log
