78b69461582fd1356c5198051ef68b94b7286e2f
jouellnyc <jouellnyc@gmail.com>
2020-03-23 20:28:37 -0400

    Update per AWS

--- flask-docker-entrypoint.sh
+++ flask-docker-entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-cd /shouldipickitup/lib
+
+cd /tmp/
+git clone https://github.com/jouellnyc/AWS.git
+cd AWS/boto3
+
+while read -r mongousername mongopassword mongohost; do
+   export MONGOUSERNAME=$mongousername  MONGOPASSWORD=$mongopassword MONGOHOST=$mongohost
+done <   <(/usr/local/bin/python3 ./getSecret.py)
+
+cd /shouldipickitup/
+sed -i s"/MONGOUSERNAME/${MONGOUSERNAME}/" lib/mongodb.py
+sed -i s"/MONGOPASSWORD/${MONGOPASSWORD}/" lib/mongodb.py
+sed -i         s"/MONGOHOST/${MONGOHOST}/" lib/mongodb.py
+
+cd lib
 ./create_data.py && touch create_data.done
+
 cd /shouldipickitup/
 /usr/local/bin/gunicorn -b 0.0.0.0:8000 app:app -w 4 --access-logfile access.log
