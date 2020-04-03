641dfe9a018a927bfe515c6dab7803e50e33d1f6
jouellnyc <jouellnyc@gmail.com>
2020-04-02 20:11:38 -0400

    ++Exceptions

--- lib/drop_db.py
+++ lib/drop_db.py
@@ -2,6 +2,10 @@
 
 import mongodb
 import logging 
+from pymongo.errors import OperationFailure
+from pymongo.errors import ConnectionFailure
+from pymongo.errors import ServerSelectionTimeoutError
+
 
 if __name__ == "__main__":
 
@@ -10,7 +14,12 @@ if __name__ == "__main__":
         drop = mg.drop_db()
         if drop is None:
             print("DB dropped OK")
+    except OperationFailure as e:
+        print('Permissions?',e)
+    except ConnectionFailure as e:
+        print("MongoDB ConnectionFailure: ", e)
+        print("Is your IP whitelisted?")
+    except ConnectionFailure as e:
+        print("Timeout",e)
     except Exception as e:
         logging.exception(e)
-
-
