e05a21eead0b5f32ccbf9e213dc57f487601a613
jouellnyc <jouellnyc@gmail.com>
2020-04-07 22:45:57 -0400

    Add gunicorn config file

--- external/gunicorn/gunicorn.conf.py
+++ external/gunicorn/gunicorn.conf.py
@@ -1,4 +1,4 @@
 workers = 4
 bind = "127.0.0.1:8000"
-access-logfile = access.log
-access_log_format = %({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"
+accesslog = 'access.log'
+access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s'
