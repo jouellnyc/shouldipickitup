42c1ad84d243d461fb8b7201dd5ea80357db2e4a
jouellnyc <jouellnyc@gmail.com>
2020-04-07 21:58:42 -0400

    Rename app to flask

--- Docker/flask-docker-entrypoint.AWS.hosted.DB.sh
+++ Docker/flask-docker-entrypoint.AWS.hosted.DB.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /shouldipickitup
-/usr/local/bin/gunicorn -b 0.0.0.0:8000 flask:app -w 4 --access-logfile access.log
+/usr/local/bin/gunicorn -b 0.0.0.0:8000 should_flask:app -w 4 --access-logfile access.log
