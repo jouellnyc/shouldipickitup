69f00c36d1c8084377ba4b655cfbbda94a94c6e1
jouellnyc <jouellnyc@gmail.com>
2020-04-07 23:54:24 -0400

    Get Real IPs from Gunicorn

--- external/gunicorn/gunicorn.conf.py
+++ external/gunicorn/gunicorn.conf.py
@@ -1,4 +1,4 @@
 workers = 4
 bind = "0.0.0.0:8000"
 accesslog = 'access.log'
-access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
