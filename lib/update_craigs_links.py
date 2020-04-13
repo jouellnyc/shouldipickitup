bdabd4eec266227c25a48dc5618bc4937dabeadc
jouellnyc <jouellnyc@gmail.com>
2020-04-12 23:55:22 -0400

    Add newyork

--- lib/update_craigs_links.py
+++ lib/update_craigs_links.py
@@ -11,7 +11,8 @@ import requestwrap
 from bs4 import BeautifulSoup
 
 all_urls     = "../non-app/all_urls.txt"
-craigs_links = "../data/craigs_links.txt.inp"
+craigs_links = "../data/craigs_links.txt"
+new_york     = "../data/new_york.txt"
 
 geo_craigs_url  = "https://geo.craigslist.org/iso/us/"
 geo_craigs_req  = requestwrap.err_web(geo_craigs_url)
@@ -24,22 +25,23 @@ with open(all_urls,'w') as fh:
         if 'www' not in url  and  'apple.com' not in url and 'google' not  in url and 'forums' not in url:
             fh.write(f"{city}={url}\n")
 
-file_pos = 0
 with open(all_urls,'r') as fh:
     with open(craigs_links,'w', buffering=1) as fh2:
-	    for line in fh:
-                file_pos += len(line)
-                print(file_pos)
-                citystate, url = line.split('=')
-                if ',' not in line:
-                   if 'newyorkcity' in citystate:
-                       continue
-                   url = url.rstrip()
-                   try:
-                       c,s = crawler.get_city_from_first_free_cl_item(url)
-                   except TypeError as e:
-                       print("no data for", url)
-                   else:
-                       fh2.write(f"{c},{s}={url}\n")
+        for line in fh:
+            citystate, url = line.split('=')
+            if ',' not in line:
+                if 'newyorkcity' in citystate:
+                    continue
+                url = url.rstrip()
+                try:
+                    c,s = crawler.get_city_from_first_free_cl_item(url)
+                except TypeError as e:
+                    print("no data for", url)
                 else:
-                    fh2.write(f"{citystate}={url}")
+                    fh2.write(f"{c},{s}={url}\n")
+            else:
+                fh2.write(f"{citystate}={url}")
+        with open(new_york,'r') as fh3:
+            for line in fh:
+                fh2.write(line)
+
